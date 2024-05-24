1c64bef66c9d48b9c8ff9d0a8e41ec27f1ab5b48
Sourabh1414 <122515981+Sourabh1414@users.noreply.github.com>
2024-05-24 10:21:14 +0530

    Update main_1.py

--- virtual_mouse/Pranavi/main_1.py
+++ virtual_mouse/Pranavi/main_1.py
@@ -1,73 +1,130 @@
 import cv2
-import numpy as np
-import HandTrackingModule as htm
-import time
-import autopy
+import mediapipe as mp
+import pyautogui
+import random
+import util
+from pynput.mouse import Button, Controller
+mouse = Controller()
 
-##########################
-wCam, hCam = 640, 480
-frameR = 100 # Frame Reduction
-smoothening = 7
-#########################
 
-pTime = 0
-plocX, plocY = 0, 0
-clocX, clocY = 0, 0
+screen_width, screen_height = pyautogui.size()
 
-cap = cv2.VideoCapture(1)
-cap.set(3, wCam)
-cap.set(4, hCam)
-detector = htm.handDetector(maxHands=1)
-wScr, hScr = autopy.screen.size()
-# print(wScr, hScr)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(
+    static_image_mode=False,
+    model_complexity=1,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.7,
+    max_num_hands=1
+)
 
-while True:
-    # 1. Find hand Landmarks
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList, bbox = detector.findPosition(img)
-    # 2. Get the tip of the index and middle fingers
-    if len(lmList) != 0:
-        x1, y1 = lmList[8][1:]
-        x2, y2 = lmList[12][1:]
-        # print(x1, y1, x2, y2)
-    
-    # 3. Check which fingers are up
-    fingers = detector.fingersUp()
-    # print(fingers)
-    cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
-    (255, 0, 255), 2)
-    # 4. Only Index Finger : Moving Mode
-    if fingers[1] == 1 and fingers[2] == 0:
-        # 5. Convert Coordinates
-        x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
-        y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
-        # 6. Smoothen Values
-        clocX = plocX + (x3 - plocX) / smoothening
-        clocY = plocY + (y3 - plocY) / smoothening
-    
-        # 7. Move Mouse
-        autopy.mouse.move(wScr - clocX, clocY)
-        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-        plocX, plocY = clocX, clocY
-        
-    # 8. Both Index and middle fingers are up : Clicking Mode
-    if fingers[1] == 1 and fingers[2] == 1:
-        # 9. Find distance between fingers
-        length, img, lineInfo = detector.findDistance(8, 12, img)
-        print(length)
-        # 10. Click mouse if distance short
-        if length < 40:
-            cv2.circle(img, (lineInfo[4], lineInfo[5]),
-            15, (0, 255, 0), cv2.FILLED)
-            autopy.mouse.click()
-    
-    # 11. Frame Rate
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3,
-    (255, 0, 0), 3)
-    # 12. Display
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+
+def find_finger_tip(processed):
+    if processed.multi_hand_landmarks:
+        hand_landmarks = processed.multi_hand_landmarks[0]  # Assuming only one hand is detected
+        index_finger_tip = hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP]
+        return index_finger_tip
+    return None, None
+
+
+def move_mouse(index_finger_tip):
+    if index_finger_tip is not None:
+        x = int(index_finger_tip.x * screen_width)
+        y = int(index_finger_tip.y / 2 * screen_height)
+        pyautogui.moveTo(x, y)
+
+
+def is_left_click(landmark_list, thumb_index_dist):
+    return (
+            util.get_angle(landmark_list[5], landmark_list[6], landmark_list[8]) < 50 and
+            util.get_angle(landmark_list[9], landmark_list[10], landmark_list[12]) > 90 and
+            thumb_index_dist > 50
+    )
+
+
+def is_right_click(landmark_list, thumb_index_dist):
+    return (
+            util.get_angle(landmark_list[9], landmark_list[10], landmark_list[12]) < 50 and
+            util.get_angle(landmark_list[5], landmark_list[6], landmark_list[8]) > 90  and
+            thumb_index_dist > 50
+    )
+
+
+def is_double_click(landmark_list, thumb_index_dist):
+    return (
+            util.get_angle(landmark_list[5], landmark_list[6], landmark_list[8]) < 50 and
+            util.get_angle(landmark_list[9], landmark_list[10], landmark_list[12]) < 50 and
+            thumb_index_dist > 50
+    )
+
+
+def is_screenshot(landmark_list, thumb_index_dist):
+    return (
+            util.get_angle(landmark_list[5], landmark_list[6], landmark_list[8]) < 50 and
+            util.get_angle(landmark_list[9], landmark_list[10], landmark_list[12]) < 50 and
+            thumb_index_dist < 50
+    )
+
+
+def detect_gesture(frame, landmark_list, processed):
+    if len(landmark_list) >= 21:
+
+        index_finger_tip = find_finger_tip(processed)
+        thumb_index_dist = util.get_distance([landmark_list[4], landmark_list[5]])
+
+        if util.get_distance([landmark_list[4], landmark_list[5]]) < 50  and util.get_angle(landmark_list[5], landmark_list[6], landmark_list[8]) > 90:
+            move_mouse(index_finger_tip)
+        elif is_left_click(landmark_list,  thumb_index_dist):
+            mouse.press(Button.left)
+            mouse.release(Button.left)
+            cv2.putText(frame, "Left Click", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        elif is_right_click(landmark_list, thumb_index_dist):
+            mouse.press(Button.right)
+            mouse.release(Button.right)
+            cv2.putText(frame, "Right Click", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        elif is_double_click(landmark_list, thumb_index_dist):
+            pyautogui.doubleClick()
+            cv2.putText(frame, "Double Click", (50, 50), FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+        elif is_screenshot(landmark_list,thumb_index_dist ):
+            im1 = pyautogui.screenshot()
+            label = random.randint(1, 1000)
+            im1.save(f'my_screenshot_{label}.png')
+            cv2.putText(frame, "Screenshot Taken", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+
+
+def main():
+    draw = mp.solutions.drawing_utils
+    cap = cv2.VideoCapture(0)
+
+    try:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = cv2.flip(frame, 1)
+            frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            processed = hands.process(frameRGB)
+
+            landmark_list = []
+            if processed.multi_hand_landmarks:
+                hand_landmarks = processed.multi_hand_landmarks[0]  # Assuming only one hand is detected
+                draw.draw_landmarks(frame, hand_landmarks, mpHands.HAND_CONNECTIONS)
+                for lm in hand_landmarks.landmark:
+                    landmark_list.append((lm.x, lm.y))
+
+            detect_gesture(frame, landmark_list, processed)
+
+            cv2.imshow('Frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
